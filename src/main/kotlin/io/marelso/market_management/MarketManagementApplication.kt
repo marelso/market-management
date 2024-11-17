6a929c97a92003b2444f9f1b2d99836c35faa62b
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-17 15:53:28 -0300

    Initial create

---
+++ src/main/kotlin/io/marelso/market_management/MarketManagementApplication.kt
@@ -0,0 +1,11 @@
+package io.marelso.market_management
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<MarketManagementApplication>(*args)
+}
---
+++ src/test/kotlin/io/marelso/market_management/MarketManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.marelso.market_management
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MarketManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
