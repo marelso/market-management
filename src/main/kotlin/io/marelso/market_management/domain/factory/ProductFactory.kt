7f55de156c1bf06335b4e8bbfb9fae13e3f6880d
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-26 01:42:32 -0300

    Including product factory to handle entity conversion

---
+++ src/main/kotlin/io/marelso/market_management/domain/factory/ProductFactory.kt
@@ -0,0 +1,31 @@
+package io.marelso.market_management.domain.factory
+
+import io.marelso.market_management.domain.Product
+import io.marelso.market_management.domain.dto.CreateProductDto
+import io.marelso.market_management.domain.dto.ProductDto
+import org.springframework.stereotype.Component
+
+@Component
+class ProductFactory {
+    fun from(create: CreateProductDto): Product {
+        return Product(
+            name = create.name,
+            storeId = create.storeId,
+            description = create.description,
+            price = create.price,
+            pictureUrl = create.pictureUrl
+        )
+    }
+
+    fun from(products: List<Product>) = products.map { from(it) }
+
+    fun from(product: Product): ProductDto {
+        return ProductDto(
+            id = product.id,
+            name = product.name,
+            description = product.description,
+            price = product.price,
+            pictureUrl = product.pictureUrl
+        )
+    }
+}
