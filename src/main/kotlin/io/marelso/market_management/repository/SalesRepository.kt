d571b1d02add117f7a637597ecccbdef010b376f
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-26 03:11:31 -0300

    Including sales repository and sales service to handle product transactions

--- src/main/kotlin/io/marelso/market_management/repository/SalesRepository.kt
+++ src/main/kotlin/io/marelso/market_management/repository/SalesRepository.kt
@@ -3,7 +3,10 @@ package io.marelso.market_management.repository
 import io.marelso.market_management.domain.Sale
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface SalesRepository: MongoRepository<Sale, String> {
+    fun findFirstByProductId(productId: String): Optional<Sale>
+    fun countSalesByProductId(productId: String): Int
 }
---
+++ src/main/kotlin/io/marelso/market_management/service/SalesService.kt
@@ -0,0 +1,51 @@
+package io.marelso.market_management.service
+
+import io.marelso.market_management.domain.Sale
+import io.marelso.market_management.domain.dto.BuyProductDto
+import io.marelso.market_management.domain.factory.SalesFactory
+import io.marelso.market_management.repository.SalesRepository
+import org.springframework.context.annotation.Lazy
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+
+@Service
+class SalesService(
+    private val repository: SalesRepository,
+    private val factory: SalesFactory,
+    @Lazy private val productService: ProductService
+) {
+    fun buy(buy: BuyProductDto) {
+        buy.products.forEach { product ->
+            val productEntity = productService.getById(product.productId)
+
+            for (index in 1..product.quantity) {
+                repository.save(factory.from(product, productEntity.storeId))
+            }
+        }
+    }
+
+    fun sell(sell: BuyProductDto) {
+        sell.products.forEach { product ->
+            val entity = getSaleByProductId(product.productId)
+            val productEntity = productService.getById(product.productId)
+
+            for (index in 1..product.quantity) {
+                repository.save(
+                    entity.copy(
+                        sellingPrice = productEntity.price,
+                        sellingDate = LocalDateTime.now(),
+                        receiptUrl = sell.invoiceUrl,
+                    )
+                )
+            }
+        }
+    }
+
+    fun countSalesByProductId(productId: String): Int = repository.countSalesByProductId(productId)
+
+    private fun getSaleByProductId(id: String): Sale {
+        return repository.findFirstByProductId(id).orElseThrow {
+            throw RuntimeException("There are no sales for given product id: $id")
+        }
+    }
+}
