73bb65f397a6aadec8185074ffa5b8afe4205d96
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-26 02:22:23 -0300

    Enabling mongo audit

---
+++ src/main/kotlin/io/marelso/market_management/config/MongoConfig.kt
@@ -0,0 +1,8 @@
+package io.marelso.market_management.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.config.EnableMongoAuditing
+
+@Configuration
+@EnableMongoAuditing
+class MongoConfig
