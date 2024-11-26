5149c9afc01f1d77cd84a75556fa7834d5f1c355
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-26 03:11:50 -0300

    Including product counts

--- src/main/kotlin/io/marelso/market_management/service/ProductService.kt
+++ src/main/kotlin/io/marelso/market_management/service/ProductService.kt
@@ -1,5 +1,6 @@
 package io.marelso.market_management.service
 
+import io.marelso.market_management.domain.Product
 import io.marelso.market_management.domain.dto.CreateProductDto
 import io.marelso.market_management.domain.dto.ProductDto
 import io.marelso.market_management.domain.factory.ProductFactory
@@ -10,25 +11,31 @@ import org.springframework.stereotype.Service
 @Service
 class ProductService(
     private val repository: ProductRepository,
-    private val factory: ProductFactory
+    private val factory: ProductFactory,
+    private val salesService: SalesService
 ) {
     fun create(product: CreateProductDto): ProductDto {
-        return factory.from(repository.save(factory.from(product)))
+        val entity = repository.save(factory.from(product))
+        return factory.from(entity, salesService.countSalesByProductId(entity.id.orEmpty()))
     }
 
     fun getByStoreId(storeId: String, pageable: Pageable): List<ProductDto> {
-        return factory.from(repository.findAllByStoreId(storeId, pageable))
+        return factory.from(repository.findAllByStoreId(storeId, pageable).map { product: Product ->
+            Pair(product, salesService.countSalesByProductId(product.id.orEmpty()))
+        })
     }
 
     fun getById(id: String): ProductDto {
-        return factory.from(findById(id))
+        val entity = findById(id)
+        return factory.from(entity, salesService.countSalesByProductId(entity.id.orEmpty()))
     }
 
     private fun findById(id: String) = repository.findById(id).orElseThrow {
-        throw RuntimeException("Store with id $id not found")
+        throw RuntimeException("Product with id $id not found")
     }
 
     fun update(id: String, update: CreateProductDto): ProductDto {
-        return factory.from(repository.save(factory.from(findById(id), update)))
+        val entity = repository.save(factory.from(findById(id), update))
+        return factory.from(entity, salesService.countSalesByProductId(entity.id.orEmpty()))
     }
 }
