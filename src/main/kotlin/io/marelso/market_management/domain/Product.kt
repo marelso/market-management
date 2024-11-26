7f5575cfd5bd32ac82bdb6ca7cdd922493c49074
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-26 02:21:41 -0300

    Including new properties to handle prodcut dates

--- src/main/kotlin/io/marelso/market_management/domain/Product.kt
+++ src/main/kotlin/io/marelso/market_management/domain/Product.kt
@@ -1,10 +1,16 @@
 package io.marelso.market_management.domain
 
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
+
 data class Product(
     val id: String? = null,
     val storeId: String,
     val name: String,
     val price: Double,
     val pictureUrl: String? = null,
-    val description: String? = null
+    val description: String? = null,
+    @CreatedDate val createdDate: LocalDateTime = LocalDateTime.now(),
+    @LastModifiedDate val lastModifiedDate: LocalDateTime = LocalDateTime.now()
 )
