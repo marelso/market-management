0f7123a41fa5ae37adc441fed8df548525bdb055
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-26 01:41:11 -0300

    Updating product data model

--- src/main/kotlin/io/marelso/market_management/domain/Product.kt
+++ src/main/kotlin/io/marelso/market_management/domain/Product.kt
@@ -2,8 +2,9 @@ package io.marelso.market_management.domain
 
 data class Product(
     val id: String? = null,
+    val storeId: String,
+    val name: String,
+    val price: Double,
     val pictureUrl: String? = null,
-    val name: String? = null,
-    val description: String? = null,
-    val price: Double? = null
+    val description: String? = null
 )
