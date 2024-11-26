b19c97d7292bf6ea9bd78bad6239dd617da0618b
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-26 00:57:54 -0300

    Including new data model to handle product entity

---
+++ src/main/kotlin/io/marelso/market_management/domain/Product.kt
@@ -0,0 +1,9 @@
+package io.marelso.market_management.domain
+
+data class Product(
+    val id: String? = null,
+    val pictureUrl: String? = null,
+    val name: String? = null,
+    val description: String? = null,
+    val price: Double? = null
+)
