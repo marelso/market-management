1291137799cf9ef4c9c6b4df4c1bf3805d03a3de
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-26 01:41:36 -0300

    Including method to fetch products by store id

---
+++ src/main/kotlin/io/marelso/market_management/repository/ProductRepository.kt
@@ -0,0 +1,11 @@
+package io.marelso.market_management.repository
+
+import io.marelso.market_management.domain.Product
+import org.springframework.data.domain.Pageable
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProductRepository: MongoRepository<Product, String> {
+    fun findAllByStoreId(storeId: String, pageable: Pageable): List<Product>
+}
