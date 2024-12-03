2c21faf988434ae1eab8bda0b35b3118f0426954
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-12-03 02:08:02 -0300

    Including new repository to handle transactions

--- src/main/kotlin/io/marelso/market_management/repository/TransactionRepository.kt
+++ src/main/kotlin/io/marelso/market_management/repository/TransactionRepository.kt
@@ -1,4 +1,9 @@
 package io.marelso.market_management.repository
 
-class TransactionRepository {
+import io.marelso.market_management.domain.Transaction
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface TransactionRepository: MongoRepository<Transaction, String> {
 }
