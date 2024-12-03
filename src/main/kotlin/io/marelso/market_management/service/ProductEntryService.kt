9ed1b3b8a2d009f0d691060b779c65dbd6d6bfe0
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-12-03 02:08:40 -0300

    Removing condition check to keep code more conciese

--- src/main/kotlin/io/marelso/market_management/service/ProductEntryService.kt
+++ src/main/kotlin/io/marelso/market_management/service/ProductEntryService.kt
@@ -1,11 +1,9 @@
 package io.marelso.market_management.service
 
 import io.marelso.market_management.domain.ProductEntry
-import io.marelso.market_management.domain.Transaction
 import io.marelso.market_management.domain.dto.ProductDto
 import io.marelso.market_management.domain.dto.SaleProductDto
 import io.marelso.market_management.repository.ProductEntryRepository
-import io.marelso.market_management.repository.ProductRepository
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,25 +12,23 @@ class ProductEntryService(
     private val productService: ProductService
 ) {
     fun create(
-        saleProductDto: SaleProductDto,
+        cart: SaleProductDto,
         transactionId: String,
         accountId: String,
         storeId: String
     ): ProductDto {
-        if(productService.existsById(saleProductDto.productId)) {
-            repository.save(
-                ProductEntry(
-                    transactionId = transactionId,
-                    productId = saleProductDto.productId,
-                    storeId = storeId,
-                    accountId = accountId,
-                    cost = saleProductDto.cost,
-                )
-            )
+        productService.existsById(cart.productId)
 
-            return productService.getById(saleProductDto.productId)
-        } else {
-            throw RuntimeException("Product with id ${saleProductDto.productId} not found")
-        }
+        repository.save(
+            ProductEntry(
+                transactionId = transactionId,
+                productId = cart.productId,
+                storeId = storeId,
+                accountId = accountId,
+                price = cart.cost,
+            )
+        )
+
+        return productService.getById(cart.productId)
     }
 }
