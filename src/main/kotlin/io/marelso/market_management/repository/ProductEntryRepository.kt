65321c38314249de9dbb47244cc87433d3190497
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-12-06 01:56:44 -0300

    Including method to return first product not deleted

--- src/main/kotlin/io/marelso/market_management/repository/ProductEntryRepository.kt
+++ src/main/kotlin/io/marelso/market_management/repository/ProductEntryRepository.kt
@@ -3,7 +3,9 @@ package io.marelso.market_management.repository
 import io.marelso.market_management.domain.ProductEntry
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface ProductEntryRepository: MongoRepository<ProductEntry, String> {
+    fun findFirstByProductIdAndDeletedFalse(productId: String): Optional<ProductEntry>
 }
